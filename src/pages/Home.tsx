c8467364ef9c8eaee1959b96b628f75d25a19d7d
Dchole <derekoware47@gmail.com>
2021-10-06 10:55:30 +0000

    use tile component at homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { card } from "@/data/card"
 import { Typography } from "@mui/material"
 import { Box } from "@mui/system"
-import { Link } from "react-router-dom"
+import Tile from "~/Tile"
 
 const Home = () => {
   return (
@@ -17,27 +17,7 @@ const Home = () => {
         </Typography>
         <Box display="flex" gap={3} overflow="scroll" px={3} pt={2} pb={3}>
           {[1, 2, 3].map(i => (
-            <Box
-              component={Link}
-              to={card.path}
-              key={i}
-              width="fit-content"
-              display="flex"
-              gap={1.2}
-              flexDirection="column"
-              sx={{ textDecoration: "none" }}
-            >
-              <img
-                src={card.cover}
-                alt={card.title}
-                loading="lazy"
-                width="140"
-                height="140"
-              />
-              <Typography align="center" color="textSecondary">
-                {card.title}
-              </Typography>
-            </Box>
+            <Tile key={i} title={card.title} type="album" />
           ))}
         </Box>
       </Box>
@@ -52,22 +32,7 @@ const Home = () => {
         </Typography>
         <Box display="flex" gap={3} overflow="scroll" px={3} pt={2} pb={3}>
           {[1, 2, 3].map(i => (
-            <Box
-              key={i}
-              width="fit-content"
-              display="flex"
-              gap={1.2}
-              flexDirection="column"
-            >
-              <img
-                src={card.cover}
-                alt={card.title}
-                loading="lazy"
-                width="140"
-                height="140"
-              />
-              <Typography align="center">{card.title}</Typography>
-            </Box>
+            <Tile key={i} title={card.title} type="album" />
           ))}
         </Box>
       </Box>
