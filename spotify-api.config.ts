62bf44981d18deba9e9fc48592cfae518f7f7aa7
Dchole <derekoware47@gmail.com>
2021-10-10 10:18:28 +0000

    update redirect uri

--- spotify-api.config.ts
+++ spotify-api.config.ts
@@ -3,5 +3,8 @@ import SpotifyWebApi from "spotify-web-api-node"
 export const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: "http://localhost:3000/auth"
+  redirectUri:
+    process.env.NODE_ENV === "production"
+      ? "https://spotify-pied-one.vercel.app/auth"
+      : "http://localhost:3000/auth"
 })
