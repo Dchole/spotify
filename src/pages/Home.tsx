467783d531a06ee1c0100d9e173b9bb33e004ba1
Dchole <derekoware47@gmail.com>
2021-10-15 18:53:48 +0000

    recommendation and new releases

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import {
+  useGetNewReleasesQuery,
   useGetPlaylistsQuery,
   useGetRecentlyPlayedQuery,
+  useGetRecommendationsQuery,
   useGetTopTracksQuery
 } from "@/generated/graphql"
 import { slugify } from "@/utils"
@@ -8,44 +10,54 @@ import { Typography } from "@mui/material"
 import { Box } from "@mui/system"
 import Tile from "~/Tile"
 
-const collections = ["my playlists", "top tracks", "recently played"]
+const collections = [
+  "my recommendations",
+  "new releases",
+  "my playlists",
+  "top tracks",
+  "recently played"
+]
 
 const Home = () => {
+  const recommendations = useGetRecommendationsQuery().data?.recommendation
+  const newReleases = useGetNewReleasesQuery().data?.new_releases
   const playlists = useGetPlaylistsQuery().data?.playlists
-  const topTracks = useGetTopTracksQuery().data?.top_tracks
   const recentlyPlayed = useGetRecentlyPlayedQuery().data?.recently_played
+  const topTracks = useGetTopTracksQuery().data?.top_tracks
 
   return (
     <Box component="main" width="100%">
-      {[playlists, topTracks, recentlyPlayed].map((collection, index) => (
-        <Box
-          key={index}
-          id={`${slugify(collections[index])}`}
-          component="section"
-          sx={{ mb: 3 }}
-          aria-labelledby={`${slugify(collections[index])}-heading`}
-        >
-          <Typography
-            id={`${slugify(collections[index])}-heading`}
-            variant="h4"
-            component="h2"
-            sx={{ pl: 2, textTransform: "capitalize" }}
+      {[recommendations, newReleases, playlists, topTracks, recentlyPlayed].map(
+        (collection, index) => (
+          <Box
+            key={index}
+            id={`${slugify(collections[index])}`}
+            component="section"
+            sx={{ mb: 3 }}
+            aria-labelledby={`${slugify(collections[index])}-heading`}
           >
-            {collections[index]}
-          </Typography>
-          <Box display="flex" gap={3} overflow="scroll" px={3} pt={2} pb={3}>
-            {collection?.map(({ id, name, type, cover_image }) => (
-              <Tile
-                key={id}
-                id={id}
-                name={name}
-                type={type}
-                cover_image={cover_image}
-              />
-            ))}
+            <Typography
+              id={`${slugify(collections[index])}-heading`}
+              variant="h4"
+              component="h2"
+              sx={{ pl: 2, textTransform: "capitalize" }}
+            >
+              {collections[index]}
+            </Typography>
+            <Box display="flex" gap={3} overflow="scroll" px={3} pt={2} pb={3}>
+              {collection?.map(({ id, name, type, cover_image }) => (
+                <Tile
+                  key={id}
+                  id={id}
+                  name={name}
+                  type={type}
+                  cover_image={cover_image}
+                />
+              ))}
+            </Box>
           </Box>
-        </Box>
-      ))}
+        )
+      )}
     </Box>
   )
 }
