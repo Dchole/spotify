bfcc6003952edb2c0c912f143a431e33de35d66c
Dchole <derekoware47@gmail.com>
2021-10-15 19:26:51 +0000

    remove react-query packages

--- spotify-api.config.ts
+++
@@ -1,7 +0,0 @@
-import SpotifyWebApi from "spotify-web-api-node"
-
-export const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.SPOTIFY_REDIRECT_URI
-})
